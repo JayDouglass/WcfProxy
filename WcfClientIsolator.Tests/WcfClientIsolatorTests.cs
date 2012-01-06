2135ce2f0bbe7643ccb6c8dfb0b7d46ab3a3bb8c
unknown <jay.douglass@gmail.com>
2012-01-05 22:14:24 -0600

    Broke out classes into seperate files

--- WcfClientIsolator.Tests/WcfClientIsolatorTests.cs
+++ WcfClientIsolator.Tests/WcfClientIsolatorTests.cs
@@ -1,11 +1,12 @@
 ﻿extern alias CastleDll;
+extern alias MoqDll;
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
-using Moq;
+using MoqDll.Moq;
 using System.ServiceModel;
 using ProxyGenerator = CastleDll.Castle.DynamicProxy.ProxyGenerator;
 using IInterceptor = CastleDll.Castle.DynamicProxy.IInterceptor;
@@ -150,7 +151,7 @@ namespace WcfClientIsolator.Tests
         public void can_inject_mock_service_implementation_into_service_host()
         {
             // Arrange
-            Castle.DynamicProxy.Generators.
+            MoqDll.Castle.DynamicProxy.Generators.
                 AttributesToAvoidReplicating.Add(typeof(ServiceContractAttribute));
             var helloWorldService = new Mock<IHelloWorldService>();
             helloWorldService.Setup(s => s.HelloWorld()).Returns("mocked result");
@@ -170,134 +171,6 @@ namespace WcfClientIsolator.Tests
             service.Close();
 
             Assert.Equal("mocked result", result);
-        }
-    }
-
-    public class WcfProxyFactory
-    {
-        private readonly ProxyGenerator generator = new ProxyGenerator();
-
-        public T Create<T>(Func<object> createChannel)
-        {
-            var channelManager = new WcfChannelManager(createChannel);
-            var interceptor = new WcfInterceptor(channelManager);
-            var interfaces = new Type[] { typeof(T), typeof(IClientChannel) };
-            var proxy = generator.CreateInterfaceProxyWithTarget(typeof(IWcfChannelManager), interfaces, channelManager, interceptor);
-            return (T)proxy;
-        }
-    }
-
-    public interface IWcfChannelManager
-    {
-        IChannel Channel { get; }
-    }
-
-    public class WcfChannelManager : IWcfChannelManager
-    {
-        readonly Func<object> createChannel;
-        public IChannel Channel { get; private set; }
-        int disposed;
-
-        public WcfChannelManager(Func<object> createChannel)
-        {
-            this.createChannel = createChannel;
-        }
-
-        public void Invoke(IInvocation invocation)
-        {
-            RefreshChannel();
-
-            try
-            {
-                var result = invocation.Method.Invoke(Channel, invocation.Arguments);
-                invocation.ReturnValue = result;
-            }
-            catch (TargetInvocationException ex)
-            {
-                throw ex.InnerException;
-            }
-        }
-
-        public bool IsChannelUsable
-        {
-            get
-            {
-                switch (Channel.State)
-                {
-                    case CommunicationState.Closed:
-                    case CommunicationState.Closing:
-                    case CommunicationState.Faulted:
-                        return false;
-                }
-                return true;
-            }
-        }
-
-        private void CreateChannel()
-        {
-            Channel = (IChannel)createChannel();
-        }
-
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        public void RefreshChannel()
-        {
-            if (disposed == 0 && (Channel == null || IsChannelUsable == false))
-            {
-                if (Channel != null)
-                {
-                    ReleaseChannel();
-                }
-
-                CreateChannel();
-            }
-        }
-
-        private void ReleaseChannel()
-        {
-            bool success = false;
-            try
-            {
-                if (Channel.State != CommunicationState.Faulted)
-                {
-                    Channel.Close();
-                    success = true;
-                }
-            }
-            finally
-            {
-                if (!success)
-                {
-                    Channel.Abort();
-                }
-            }
-        }
-
-        public void Dispose()
-        {
-            if (Interlocked.CompareExchange(ref disposed, 1, 0) == 1)
-                return;
-
-            if (Channel != null)
-                ReleaseChannel();
-        }
-    }
-
-    public class WcfInterceptor : IInterceptor
-    {
-        WcfChannelManager channelManager;
-
-        public WcfInterceptor(WcfChannelManager channelManager)
-        {
-            this.channelManager = channelManager;
-        }
-
-        public void Intercept(IInvocation invocation)
-        {
-            if (invocation.Method.DeclaringType.Name == "IDisposable"
-                && invocation.Method.Name == "Dispose")
-                channelManager.Dispose();
-
-            channelManager.Invoke(invocation);
         }
     }
 }
