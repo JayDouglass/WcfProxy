efbf2a9ceb8d8b4adb664379a49a076a5d4ea03f
unknown <jay.douglass@gmail.com>
2012-01-05 22:53:40 -0600

    Renamed project folder

---
+++ WcfProxy.Tests/WcfInterceptor.cs
@@ -0,0 +1,30 @@
+﻿extern alias CastleDll;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IInterceptor = CastleDll.Castle.DynamicProxy.IInterceptor;
+using IInvocation = CastleDll.Castle.DynamicProxy.IInvocation;
+
+namespace WcfClientIsolator.Tests
+{
+    public class WcfInterceptor : IInterceptor
+    {
+        WcfChannelManager channelManager;
+
+        public WcfInterceptor(WcfChannelManager channelManager)
+        {
+            this.channelManager = channelManager;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            if (invocation.Method.DeclaringType.Name == "IDisposable"
+                && invocation.Method.Name == "Dispose")
+                channelManager.Dispose();
+
+            channelManager.Invoke(invocation);
+        }
+    }
+}
